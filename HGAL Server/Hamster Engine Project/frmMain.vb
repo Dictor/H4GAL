e2f58c16b572ed94573ac7870cff5c86b5f239b3
KimJeongHyun <kimdictor@gmail.com>
2018-01-22 22:43:08 +0900

    업데이트된 소켓 라이브러리에 맞게 콜백함수 수정

--- HGAL Server/Hamster Engine Project/frmMain.vb
+++ HGAL Server/Hamster Engine Project/frmMain.vb
@@ -1,4 +1,6 @@
-﻿Public Class frmMain
+﻿Imports System.Reflection
+
+Public Class frmMain
     Private Class SessionData
         Public ID As String
         Public MadeTime As Date
@@ -11,6 +13,7 @@
     End Sub
 
     Private ServerSoc As Object
+    Delegate Sub socevt(kind As Object, args() As Object)
     Public Sub initproc()
         Print("UI 로드 완료")
         'Print(Engine.ProjVersion.GetName & "  " & Engine.ProjVersion.GetVersion(True))
@@ -18,18 +21,25 @@
 
         Try
             ServerSoc = Project.Hamsoc.CopyMe()
-            Print("0.0.0.0:81에서 Listen모드로 초기화합니다.")
-            ServerSoc.Init(True, "0.0.0.0", 81, 255)
+            Print("0.0.0.0:81에서 Listen모드로 소켓을 초기화합니다.")
+            ServerSoc.Init(True, "0.0.0.0", 81, New socevt(AddressOf SocListen))
             Print("Listen을 시작합니다")
-            ServerSoc.SetListen
+            ServerSoc.SetListen()
         Catch ex As Exception
             Print("소켓을 초기화하는중 오류가 발생했습니다!!")
             Print(ex.ToString)
+            MsgBox(ex.ToString)
         End Try
     End Sub
 
-    Public Sub SocListen(kind As Integer, args() As Object)
-
+    Public Sub SocListen(kind As Object, args() As Object)
+        Print(kind.ToString & " / " & args(0).ToString)
+        If kind = 0 Then 'Connect
+        ElseIf kind = 1 Then 'ConnectListen
+        ElseIf kind = 2 Then 'Disconnect
+        ElseIf kind = 3 Then 'Listen
+            Print(System.Text.Encoding.UTF8.GetString(args(0)))
+        End If
     End Sub
 
     Public Sub Print(data As String)
