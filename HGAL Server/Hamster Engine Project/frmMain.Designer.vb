b0e16ea76e786db03b37e9af04223161794a7db4
KimJeongHyun <kimdictor@gmail.com>
2018-06-16 23:12:54 +0900

    서버 UI 일부 변경 (총 전송 텍스트 량 표시 추가)

--- HGAL Server/Hamster Engine Project/frmMain.Designer.vb
+++ HGAL Server/Hamster Engine Project/frmMain.Designer.vb
@@ -23,6 +23,7 @@ Partial Class frmMain
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.lstLog = New System.Windows.Forms.ListBox()
+        Me.txtSentBytes = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lstLog
@@ -31,23 +32,37 @@ Partial Class frmMain
         Me.lstLog.FormattingEnabled = True
         Me.lstLog.HorizontalExtent = 700
         Me.lstLog.HorizontalScrollbar = True
+        Me.lstLog.ItemHeight = 21
         Me.lstLog.Location = New System.Drawing.Point(13, 13)
         Me.lstLog.Name = "lstLog"
-        Me.lstLog.Size = New System.Drawing.Size(793, 381)
+        Me.lstLog.Size = New System.Drawing.Size(793, 361)
         Me.lstLog.TabIndex = 0
+        '
+        'txtSentBytes
+        '
+        Me.txtSentBytes.AutoSize = True
+        Me.txtSentBytes.Font = New System.Drawing.Font("맑은 고딕", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.txtSentBytes.Location = New System.Drawing.Point(12, 387)
+        Me.txtSentBytes.Name = "txtSentBytes"
+        Me.txtSentBytes.Size = New System.Drawing.Size(98, 28)
+        Me.txtSentBytes.TabIndex = 1
+        Me.txtSentBytes.Text = "총 전송 : "
         '
         'frmMain
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
-        Me.ClientSize = New System.Drawing.Size(824, 448)
+        Me.ClientSize = New System.Drawing.Size(818, 431)
+        Me.Controls.Add(Me.txtSentBytes)
         Me.Controls.Add(Me.lstLog)
         Me.MaximumSize = New System.Drawing.Size(840, 487)
         Me.MinimumSize = New System.Drawing.Size(840, 487)
         Me.Name = "frmMain"
         Me.Text = "frmMain"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents lstLog As System.Windows.Forms.ListBox
+    Friend WithEvents txtSentBytes As System.Windows.Forms.Label
 End Class
