9babac08c2d0ff3949b87f23f957d2cd645350a5
Ubuntu <ubuntu@ip-172-31-33-128.ap-northeast-1.compute.internal>
2019-11-03 06:32:30 +0000

    레거시 API SESSION, MAKESESSION 구현
    * 최대한 json 포맷 호환 유지

--- HGAL_Go_Server/main.go
+++ HGAL_Go_Server/main.go
@@ -1,23 +1,61 @@
 package main
 
 import (
-	"net/http"
+	"github.com/gorilla/sessions"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo-contrib/session"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
 	var main_server = echo.New()
+	main_server.Use(session.Middleware(sessions.NewFilesystemStore("", []byte("<TEST SECRET!!>"))))
 
-	main_server.POST("/info", )	
-	main_server.POST("/session/check", )
-	main_server.GET("/session/assign", )
-	main_server.POST("/auth/kakao/try", )
-	main_server.GET("/auth/kakao/register", )
-	main_server.POST("/auth/disp/try", )
-	main_server.GET("/auth/disp/register", )
-	main_server.GET("/list", )
-	main_server.GET("/image", )
-	ain_server.GET("/thumb", )
-	
+	//routing
+	/*main_server.POST("/info")
+	main_server.POST("/auth/kakao/try")
+	main_server.GET("/auth/kakao/register")
+	main_server.POST("/auth/disp/try")
+	main_server.GET("/auth/disp/register")
+	main_server.GET("/list")
+	main_server.GET("/image")
+	main_server.GET("/thumb")*/
+	main_server.GET("/", func(cxt echo.Context) error {
+		return cxt.String(http.StatusOK, "Hello, World!")
+	})
+	main_server.GET("/session/check", rCheckSession)
+	main_server.GET("/session/assign", rAssignSession)
+
 	main_server.Logger.Fatal(main_server.Start(":80"))
+}
+
+func rCheckSession(cxt echo.Context) error {
+	sess, _ := session.Get("session", cxt)
+	_, exist := sess.Values["credential_type"]
+	sess.Save(cxt.Request(), cxt.Response())
+	if !exist {
+		return cxt.JSON(http.StatusOK, map[string]string{"isNew": strconv.FormatBool(!exist), "error": "NOT_ASSIGNED_SESSION"})
+	} else {
+		return cxt.JSON(http.StatusOK, map[string]string{"isNew": strconv.FormatBool(!exist), "status": sess.Values["credential_type"].(string), "name": sess.Values["user_name"].(string)})
+	}
+}
+
+func rAssignSession(cxt echo.Context) error {
+	sess, _ := session.Get("session", cxt)
+	_, exist := sess.Values["credential_type"]
+	if !exist {
+		sess.Options = &sessions.Options{
+			Path:     "/",
+			MaxAge:   86400 * 7, //7 days
+			HttpOnly: false,
+			Secure:   false,
+		}
+		sess.Values["assigned_time"] = time.Now().Format(time.RFC3339)
+		sess.Values["credential_type"] = "empty"
+		sess.Values["user_name"] = ""
+	}
+	sess.Save(cxt.Request(), cxt.Response())
+	return cxt.JSON(http.StatusOK, map[string]string{"is_new": strconv.FormatBool(!exist), "assigned_time": sess.Values["assigned_time"].(string)})
 }
