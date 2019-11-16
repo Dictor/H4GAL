3242c1f6ced3bd24bb0000899389cb5bce0d2713
Dictor <kimdictor@gmail.com>
2019-11-16 05:17:37 +0000

    list, image 정의 및 코드 개선
    * list, image 라우팅 함수 정의
    * JSON 반환값에 string 대신 interface의 map 사용
    * 컨텍스트의 응답 데이터 가져오는 루틴 함수로 분리

--- HGAL_Go_Server/main.go
+++ HGAL_Go_Server/main.go
@@ -39,10 +39,6 @@ func main() {
 	/*main_server.POST("/info")
 	main_server.POST("/auth/kakao/try")
 	main_server.GET("/auth/kakao/register")
-
-	main_server.GET("/auth/disp/register")
-	main_server.GET("/list")
-	main_server.GET("/image")
 	main_server.GET("/thumb")*/
 	main_server.GET("/", func(cxt echo.Context) error {
 		return cxt.String(http.StatusOK, "HGAL API Server")
@@ -50,79 +46,90 @@ func main() {
 	main_server.GET("/session/check", rCheckSession)
 	main_server.GET("/session/assign", rAssignSession)
 	main_server.POST("/auth/disp/try", rTryDisposableAuth)
-
+	main_server.GET("/list", rGetImageList)
+	main_server.GET("/image", rGetImage)
+	main_server.Static("/web", "../Web")
 	main_server.Logger.Fatal(main_server.Start(":80"))
 }
 
-func rTryDisposableAuth(cxt echo.Context) error {
-	hassess := hasSession(cxt)
-	reqdata := echo.Map{}
-	if err := cxt.Bind(&reqdata); err != nil {
-		log.Print("Unexpected request bind error! : ")
-		log.Println(err)
-		return cxt.JSON(http.StatusOK, map[string]string{"status": "false", "error": "INVALID_REQUEST"})
-	}
+func rGetImageList(cxt echo.Context) error {
+}
 
-	if hassess {
-		if getSessionValue(cxt, "credential_type") == "empty" {
-			var nameok, codeok bool = false, false
-			var resname string
-
-			//Requested name validation check
-			if name, err := reqdata["name"].(string); !err {
-				return cxt.JSON(http.StatusOK, map[string]string{"status": "false", "error": "ILLEGAL_REQUEST"})
-			} else {
-				if dname, err := url.QueryUnescape(name); err == nil {
-					if utf8.RuneCountInString(dname) <= 5 {
-						nameok = true
-						resname = dname
-					} else {
-						return cxt.JSON(http.StatusOK, map[string]string{"status": "false", "error": "ILLEGAL_NAME"})
-					}
-				} else {
-					return cxt.JSON(http.StatusOK, map[string]string{"status": "false", "error": "ILLEGAL_NAME"})
-				}
-			}
-
-			//Requsted code validation and correct check
-			if _, err := reqdata["code"]; !err {
-				return cxt.JSON(http.StatusOK, map[string]string{"status": "false", "error": "ILLEGAL_REQUEST"})
-			} else {
-				for _, nowcode := range allow_dispauth_code {
-					if reqdata["code"] == nowcode {
-						codeok = true
-					}
-				}
-				if !codeok {
-					return cxt.JSON(http.StatusOK, map[string]string{"status": "false", "error": "INCORRECT_CODE"})
-				}
-			}
-
-			//All verify process passed, Assign disposable auth
-			if codeok && nameok {
-				setSessionValue(cxt, "credential_type", "disp")
-				setSessionValue(cxt, "user_name", resname)
-				return cxt.JSON(http.StatusOK, map[string]string{"status": "true"})
-			}
-		} else {
-			return cxt.JSON(http.StatusOK, map[string]string{"status": "false", "error": "ILLEGAL_CREDENTIAL"})
+func rGetImage(cxt echo.Context) error {
+	if succ, reqdata := getRequestData(cxt); succ {
+		if hassess := hasSession(cxt); hassess {
 		}
 	} else {
-		return cxt.JSON(http.StatusOK, map[string]string{"status": "false", "error": "INVALID_SESSION"})
+		return cxt.JSON(http.StatusOK, map[string]interface{}{"status": false, "error": "INVALID_REQUEST"})
 	}
-	return cxt.JSON(http.StatusOK, map[string]string{"status": "false", "error": "INTERNAL_SERVER_ERROR"}) //When all expected situation must not be reach here
+
+}
+
+func rTryDisposableAuth(cxt echo.Context) error {
+	if succ, reqdata := getRequestData(cxt); succ {
+		if hassess := hasSession(cxt); hassess {
+			if getSessionValue(cxt, "credential_type") == "empty" {
+				var nameok, codeok bool = false, false
+				var resname string
+
+				//Requested name validation check
+				if name, err := reqdata["name"].(string); !err {
+					return cxt.JSON(http.StatusOK, map[string]interface{}{"status": false, "error": "ILLEGAL_REQUEST"})
+				} else {
+					if dname, err := url.QueryUnescape(name); err == nil {
+						if utf8.RuneCountInString(dname) <= 5 {
+							nameok = true
+							resname = dname
+						} else {
+							return cxt.JSON(http.StatusOK, map[string]interface{}{"status": false, "error": "ILLEGAL_NAME"})
+						}
+					} else {
+						return cxt.JSON(http.StatusOK, map[string]interface{}{"status": false, "error": "ILLEGAL_NAME"})
+					}
+				}
+
+				//Requsted code validation and correct check
+				if _, err := reqdata["code"]; !err {
+					return cxt.JSON(http.StatusOK, map[string]interface{}{"status": false, "error": "ILLEGAL_REQUEST"})
+				} else {
+					for _, nowcode := range allow_dispauth_code {
+						if reqdata["code"].(string) == nowcode {
+							codeok = true
+						}
+					}
+					if !codeok {
+						return cxt.JSON(http.StatusOK, map[string]interface{}{"status": false, "error": "INCORRECT_CODE"})
+					}
+				}
+
+				//All verify process passed, Assign disposable auth
+				if codeok && nameok {
+					setSessionValue(cxt, "credential_type", "disp")
+					setSessionValue(cxt, "user_name", resname)
+					return cxt.JSON(http.StatusOK, map[string]interface{}{"status": true})
+				}
+			} else {
+				return cxt.JSON(http.StatusOK, map[string]interface{}{"status": false, "error": "ILLEGAL_CREDENTIAL"})
+			}
+		} else {
+			return cxt.JSON(http.StatusOK, map[string]interface{}{"status": false, "error": "INVALID_SESSION"})
+		}
+	} else {
+		return cxt.JSON(http.StatusOK, map[string]interface{}{"status": false, "error": "INVALID_REQUEST"})
+	}
+	return cxt.JSON(http.StatusOK, map[string]interface{}{"status": false, "error": "INTERNAL_SERVER_ERROR"}) //When all expected situation, code must not be reach here
 }
 
 func rCheckSession(cxt echo.Context) error {
 	hassess := hasSession(cxt)
 	if !hassess {
-		return cxt.JSON(http.StatusOK, map[string]string{"isNew": "true", "error": "NOT_ASSIGNED_SESSION"})
+		return cxt.JSON(http.StatusOK, map[string]interface{}{"is_new": true, "error": "NOT_ASSIGNED_SESSION"})
 	} else {
-		return cxt.JSON(http.StatusOK, map[string]string{"isNew": "false", "status": getSessionValue(cxt, "credential_type"), "name": getSessionValue(cxt, "user_name")})
+		return cxt.JSON(http.StatusOK, map[string]interface{}{"is_new": false, "status": getSessionValue(cxt, "credential_type"), "name": getSessionValue(cxt, "user_name")})
 	}
 }
 
-//이 함수는 세션을 직접 다루는게 효율적이라 래퍼 함수 사용하지 말기
+//이 함수는 세션을 직접 다루는게 효율적이라 래퍼 함수 사용하지 않음
 func rAssignSession(cxt echo.Context) error {
 	sess, _ := session.Get("session", cxt)
 	_, exist := sess.Values["credential_type"]
@@ -139,6 +146,16 @@ func rAssignSession(cxt echo.Context) error {
 	}
 	sess.Save(cxt.Request(), cxt.Response())
 	return cxt.JSON(http.StatusOK, map[string]string{"is_new": strconv.FormatBool(!exist), "assigned_time": sess.Values["assigned_time"].(string)})
+}
+
+func getRequestData(cxt echo.Context) (bool, map[string]interface{}) {
+	res := make(map[string]interface{})
+	if err := cxt.Bind(&res); err != nil {
+		log.Print("Unexpected request bind error! : ", err)
+		return false, nil
+	} else {
+		return true, res
+	}
 }
 
 func hasSession(cxt echo.Context) bool {
