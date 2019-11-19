d19b94d0c902d917f360cf8ecdf874b670bedeed
Dictor <kimdictor@gmail.com>
2019-11-19 11:36:10 +0000

    exploreDirectory 함수 구현

--- HGAL_Go_Server/thumbnail.go
+++ HGAL_Go_Server/thumbnail.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 )
 
 func exploreDirectory(startPath string) ([]string, error) {
 	//https://golang.org/pkg/path/filepath/#WalkFunc 로 구현
+	var result []string
+	err := filepath.Walk(startPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			result = append(result, path)
+		}
+		return nil
+	})
+	return result, err
 }
 
 func makeThumbnail(imagePath string) error {
-
+	return nil
 }
 
 func makeRecursiveThumnail(startPath string) error {
-
+	return nil
 }
